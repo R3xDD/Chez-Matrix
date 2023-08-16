bdfef7c4075c95def8e480ce5455a2a515b183b5
Youssef <youssefbriki66@gmail.com>
2023-08-16 13:56:53 +0100

    Adding the Tailwind CSS project Files(GiftCard Store)

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+let button = document.querySelector('#menu-button');
+let menu = document.querySelector('#menu');
+
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
+
+
+
+
